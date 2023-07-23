8a3e1ab21f953051e804785caa1d155efdb940f3
LanaAbo <106496115+LanaAbo@users.noreply.github.com>
2023-07-23 22:15:59 +0300

    Create RandomRecipes.vue

---
+++ src/components/RandomRecipes.vue
@@ -0,0 +1,61 @@
+<template>
+    <b-container>
+      <h3>
+        {{ title }}:
+        <slot></slot>
+      </h3>
+      <b-row>
+        <b-col v-for="r in recipes" :key="r.id">
+          <RecipePreview class="recipePreview" :recipe="r" />
+        </b-col>
+      </b-row>
+    </b-container>
+  </template>
+  
+  <script>
+  import RecipePreview from "./RecipePreview.vue";
+  export default {
+    name: "RandomRecipes",
+    components: {
+      RecipePreview
+    },
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        recipes: []
+      };
+    },
+    mounted() {
+      this.updateRecipes();
+    },
+    methods: {
+      async updateRecipes() {
+        try {
+          // console.log(this.$root.store.server_domain+"/users/recipe"1);
+          const response = await this.axios.get(
+            this.$root.store.server_domain +"/users/RandomRecipes");
+          // console.log(response);
+          const recipes = response.data;
+          this.recipes = [];
+          this.recipes.push(...recipes);
+          // console.log(this.recipes);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .container {
+    min-height: 400px;
+  }
+  
+  </style>
+  
