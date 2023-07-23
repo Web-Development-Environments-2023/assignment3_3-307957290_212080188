928ddff093dd8761739df2c52dfdbdb50f6b462b
LanaAbo <106496115+LanaAbo@users.noreply.github.com>
2023-07-23 22:15:12 +0300

    Update RecipePreviewList.vue

--- src/components/RecipePreviewList.vue
+++ src/components/RecipePreviewList.vue
@@ -6,7 +6,7 @@
     </h3>
     <b-row>
       <b-col v-for="r in recipes" :key="r.id">
-        <RecipePreview class="recipePreview" :recipe="r" />
+        <RecipePreview style="text-align:center;" class="recipePreview" :recipe="r" />
       </b-col>
     </b-row>
   </b-container>
@@ -36,13 +36,11 @@ export default {
   methods: {
     async updateRecipes() {
       try {
+        // console.log(this.$root.store.server_domain+"/users/recipe"1);
         const response = await this.axios.get(
-          this.$root.store.server_domain + "/recipes/random",
-          // "https://test-for-3-2.herokuapp.com/recipes/random"
-        );
-
+          this.$root.store.server_domain +"/users/recipe");
         // console.log(response);
-        const recipes = response.data.recipes;
+        const recipes = response.data;
         this.recipes = [];
         this.recipes.push(...recipes);
         // console.log(this.recipes);
@@ -57,5 +55,14 @@ export default {
 <style lang="scss" scoped>
 .container {
   min-height: 400px;
+  // max-height: 100px;
+  // max-width: 400px;
+  
+
+  
+
+
+
+
 }
 </style>
