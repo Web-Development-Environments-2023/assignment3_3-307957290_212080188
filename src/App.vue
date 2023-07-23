6a858c7320c6fcc7cc40220355a783b3cab5d834
LanaAbo <106496115+LanaAbo@users.noreply.github.com>
2023-07-23 22:04:32 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
-      <router-link :to="{ name: 'search' }">Search</router-link>|
-      {{ !$root.store.username }}
-      <span v-if="!$root.store.username">
-        Guest:
-        <router-link :to="{ name: 'register' }">Register</router-link>|
-        <router-link :to="{ name: 'login' }">Login</router-link>|
-      </span>
-      <span v-else>
-        {{ $root.store.username }}: <button @click="Logout">Logout</button>|
-      </span>
+    <div id="nav" style="font-style: italic;">
+      <b-navbar type="dark" variant="dark" toggleable="lg" sticky style="height: 60px; font-size: 20px;">
+        <b-navbar-nav>
+          <b-nav-item><router-link :to="{ name: 'main' }">Vue Recipes</router-link></b-nav-item>
+          <b-nav-item><router-link :to="{ name: 'search' }">Search</router-link></b-nav-item>
+          <b-nav-item><router-link :to="{ name: 'about' }">About</router-link></b-nav-item>
+          <b-nav-item v-if="$root.store.username"><router-link :to="{ name: 'newRecipe' }">New_Recipe</router-link></b-nav-item>
+
+        </b-navbar-nav>
+      <!-- {{ !$root.store.username }} -->
+      <b-navbar-nav v-if="!$root.store.username" class="ml-auto">
+        <b-nav-item style="margin-top: 0%;">Hello Guest:</b-nav-item>
+        <b-nav-item><router-link :to="{ name: 'register' }">Register</router-link></b-nav-item>
+        <b-nav-item><router-link :to="{ name: 'login' }">Login</router-link></b-nav-item>
+
+      </b-navbar-nav>
+      <b-navbar-nav v-else class="ml-auto">
+        <!-- Hello {{ $root.store.username }}: <button @click="Logout">Logout</button>|
+        <router-link :to="{ name: 'newRecipe' }">New_Recipe</router-link> -->
+
+        <b-nav-item style="margin-top: 2%;">Hello {{ $root.store.username }}:</b-nav-item>
+
+        <b-nav-item-dropdown id="dropdown-1" text="Personal" class="m-md-2">
+          <b-dropdown-item><router-link :to="{ name: 'favoritesPage' }">Favorites</router-link></b-dropdown-item>
+          <b-dropdown-item><router-link :to="{ name: 'privatePage' }">Private</router-link></b-dropdown-item>
+          <b-dropdown-item><router-link :to="{ name: 'la_FamiliaPage' }">La_Familia</router-link></b-dropdown-item>
+        </b-nav-item-dropdown>
+       
+        <b-nav-item><b-button variant="outline-info" class="mb-2" @click="Logout">
+        <b-icon icon="power" aria-hidden="true"></b-icon> Logout
+        </b-button></b-nav-item>
+
+
+      </b-navbar-nav>
+    </b-navbar>
     </div>
     <router-view />
   </div>
@@ -40,20 +63,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
   min-height: 100vh;
+  background-image:url(./assets/BackGroundImage.jpg);
+  background-attachment: fixed;
+  background-size: cover;
 }
 
 #nav {
-  padding: 30px;
+  padding: 0px;
+  color: #b5d4f3;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #b5d4f3;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #b5d4f3;
 }
 </style>
