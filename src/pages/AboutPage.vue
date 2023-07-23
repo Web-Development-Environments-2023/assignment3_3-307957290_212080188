4e8fa17181ce188f6c3391d17903130e3699a6f7
LanaAbo <106496115+LanaAbo@users.noreply.github.com>
2023-07-23 22:14:08 +0300

    Create AboutPage.vue

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2 class="about-title">About Flavor Fusion Recipes</h2>
+    <p class="about-explain">
+                This Website Was Created By Lana Aboraya And Danielle Sapir!<br>
+        Welcome to our Flavor Fusion Recipe site, a culinary paradise where you will discover a delightful variety of globally inspired dishes.<br>
+        Flavor Fusion Recipe is here to help you cook delicious meals with less stress and more joy.<br>
+        From simple family favorites to gourmet treats, our recipes are sure to tantalize your taste buds.<br> Join us on this delicious journey and let the magic of cooking be revealed in your kitchen!
+
+</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.about-title{
+    font-size: xx-large;
+    font-style: italic;
+    margin-left: 35%;
+    margin-right: 36%;
+    position: relative;
+    margin-top: 3%;
+    color: #047ca7;
+    background-color: rgb(248, 229, 242);
+    padding: 10px;
+}
+
+.about-explain{
+    font-size: large;
+    font-style: italic;
+    margin-left: 18%;
+    margin-right: 24%;
+    position: relative;
+    margin-top: 3%;
+    color: #047ca7;
+    background-color: rgb(248, 229, 242);
+    padding: 10px;
+
+}
+
+</style>
